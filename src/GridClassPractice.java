78468567f1b2cd09607fcca720ddd2990d4b547a
CadmusKei <maxwell.kei.farouk@gmail.com>
2026-03-13 20:39:36 +0200

    Finish BFS example

--- src/GridClassPractice.java
+++ src/GridClassPractice.java
@@ -6,10 +6,10 @@ public class GridClassPractice {
     private static class Grid {
 
         int[][] grid = {
-                {1, 1, 1, 1, 0},
+                {2, 1, 1, 1, 0},
                 {1, 1, 1, 0, 1},
-                {0, 0, 0, 1, 1},
-                {1, 1, 0, 0, 0}
+                {0, 0, 1, 1, 1},
+                {1, 1, 1, 0, 0}
         };
 
         int rows = grid.length;
@@ -50,39 +50,72 @@ public class GridClassPractice {
             return 1 + dfs(i -1, j) + dfs(i + 1, j) + dfs(i, j -1) + dfs(i, j + 1);
         }
 
-        private int spreadRot(){
+        private int spreadZombies(){
 
             // Create Queue and additional variables
             Queue<int[]> queue = new LinkedList<int[]>();
-            int fresh = 0;
+            int humans = 0;
             int minutes = 0;
 
-            // find Rotten
-            // Count Fresh
+            // Show Original Grid
+            displayGrid();
+
+            // find Zombies
+            // Count humans
             for (int i = 0; i < rows; i++) {
                 for(int j = 0; j < cols; j++) {
                     if (grid[i][j] == 2) queue.add(new int[]{i, j});
-                    if (grid[i][j] == 1) fresh++;
+                    if (grid[i][j] == 1) humans++;
                 }
             }
 
+            // define direcrions
             int[] dr = {-1, 1, 0, 0};
             int[] dc = {0, 0, -1, 1};
 
+            //Start simulation!
+            // As long as queue isnt empty and there are humans to infect
+            while (!queue.isEmpty() && humans > 0) {
+                // While current snapshot of queue isnt empty, spread rot based on its current size
+                int size = queue.size();
+                for (int s = 0; s < size; s++) {
 
+                    // Store zombie coords
+                    int[] zombiePos = queue.poll();
+                    int r = zombiePos[0];
+                    int c = zombiePos[1];
 
+                    for (int k = 0; k < 4; k++) {
 
+                        // Consult Zombie neighours
+                        int nr = r + dr[k];
+                        int nc = c + dc[k];
 
+                        // Infect valid neighbouring humans
+                        if (nr >= 0 && nr < rows && nc >= 0 && nc < cols && grid[nr][nc] == 1) {
+                            grid[nr][nc] = 2;
+                            humans--;
+                            queue.add(new int[]{nr, nc});
+                            displayGrid();
+                            System.out.println();
+                        }
+                    }
+                }
+                minutes++;
+            }
+            return humans == 0 ? minutes : -1;
         }
 
+        
     }
 
     public static void main(String[] args) {
         Grid grid = new Grid();
 
-        grid.displayGrid();
-
-        System.out.println(grid.maxIslandSize());
+//        grid.displayGrid();
+//        System.out.println(grid.maxIslandSize());
+//        int minutes = grid.spreadZombies();
+//        System.out.println("Map Infection took " + minutes + " minutes.");
 
     }
 }
